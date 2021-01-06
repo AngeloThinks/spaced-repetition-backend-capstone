f9914186111da4edad01b6cc1cff3a1a34dbddfd
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-06 14:26:56 -0500

    updated language

--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -172,17 +172,17 @@ describe('User Endpoints', function () {
           name: 'test name',
         }
         const expectedList = {
-          name: 'French',
+          name: 'Japanese',
           total_score: 0,
           words: [
-            { original: 'entraine toi', translation: 'practice' },
-            { original: 'bonjour', translation: 'hello' },
-            { original: 'maison', translation: 'house' },
-            { original: 'développeur', translation: 'developer' },
+            { original: 'konnichiwa', translation: 'hello' },
+            { original: 'hai', translation: 'yes' },
+            { original: 'jitaku', translation: 'home' },
+            { original: 'iie', translation: 'no' },
             { original: 'traduire', translation: 'translate' },
-            { original: 'incroyable', translation: 'amazing' },
-            { original: 'chien', translation: 'dog' },
-            { original: 'chat', translation: 'cat' },
+            { original: 'keisanki', translation: 'calculator' },
+            { original: 'inu', translation: 'dog' },
+            { original: 'neko', translation: 'cat' },
           ]
         }
         return supertest(app)
