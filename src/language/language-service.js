db07c18cdb207648148d5719f7f75123e8058ffa
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-08 17:17:25 -0500

    created linked-list-service.js

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const SLL = require('./linked-list-service')
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -28,6 +30,68 @@ const LanguageService = {
       )
       .where({ language_id })
   },
+
+  serializeWord(word, language) {
+    return {
+      nextWord: word.original,
+      totalScore: language.total_score,
+      wordCorrectCount: word.correct_count,
+      wordIncorrectCount: word.incorrect_count
+    }
+  },
+  getWord(db, id) {
+    return db
+      .from('word')
+      .select(
+        'id',
+        'language_id',
+        'original',
+        'correct_count',
+        'incorrect_count',
+        'memory_value',
+        'translation',
+        'next'
+      )
+      .where({ id })
+  },
+  serializeGuessResponse(word, language, correct) {
+    return {
+      nextWord: language.head,
+      wordCorrectCount: word.correct_count,
+      wordIncorrectCount: word.incorrect_count,
+      totalScore: language.total_score,
+      answer: word.original,
+      isCorrect: correct
+    }
+  },
+  updateWord(db, id, correct_count, incorrect_count, memory_value,next){
+    return db('word')
+    .where({ id })
+    .update({
+      correct_count,
+      memory_value,
+      incorrect_count,
+      next
+    }).returning('*')
+  },
+  updateNext(db, id, next){
+    return db('word')
+    .where({ id })
+    .update({
+      next
+    })
+    .returning('*')
+  },
+  updateScore(db, user_id, total_score, head){
+    return db('language')
+      .where({ user_id })
+      .update({
+        total_score,
+        head
+      })
+      .returning('*')
+  }
 }
 
 module.exports = LanguageService
+
