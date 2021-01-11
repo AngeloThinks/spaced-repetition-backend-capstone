ce19e89869fa7db724df621496507878ea56af2b
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-10 20:47:49 -0500

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DB_URL || 
-    'postgresql://postgres@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+      process.env.DATABASE_URL ||
+      "postgresql://postgres@localhost/spacedrepetition",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
