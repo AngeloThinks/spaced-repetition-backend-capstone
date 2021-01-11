6e669a08040ef67378142833b7c0d7fca8b38733
RX-78GP02A <mridley@knights.ucf.edu>
2021-01-10 19:15:31 -0500

    updated auth-router.js

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const AuthService = require('./auth-service')
+const express = require('express');
+const AuthService = require('./auth-service');
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const authRouter = express.Router()
