bdb24a958707d647f7ae4ebb6ccfd5126ed87f9a
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-22 01:10:29 -0600

    heroku

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { PORT, DATABASE_URL } = require("./config");
 
 const pg = require('pg');
 
-pg.defaults.ssl = process.env.NODE_ENV === "production"
+// pg.defaults.ssl = process.env.NODE_ENV === "production"
 
 const db = knex({
     client: "pg",
